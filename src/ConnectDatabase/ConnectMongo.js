1516083ed4cba988c6df3e703325b69d3898dc1c
Aaryan10-sudo <144255729+Aaryan10-sudo@users.noreply.github.com>
2024-09-03 02:00:21 +0545

    Update ConnectMongo.js

--- src/ConnectDatabase/ConnectMongo.js
+++ src/ConnectDatabase/ConnectMongo.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = () => {
-  mongoose.connect("mongodb://localhost:27017/aaryan-sharma");
+  mongoose.connect("your-mongoDB URL here");
   console.log("Connected to MongoDB ");
 };
 
